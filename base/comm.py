52546aa43118f9e951676458178f6e9eac89860f
HarrisonHall <harrisonchristianhall@gmail.com>
2018-03-08 18:00:01 -0500

    Removed struct, added json.

--- base/comm.py
+++ base/comm.py
@@ -1,8 +1,9 @@
 from __future__ import division, print_function
 
 import serial
-import struct
+#import struct
 import sys
+import json
 
 def init():
     global comm
@@ -10,21 +11,12 @@ def init():
 
 def read():
     try:
-        header = comm.read(5)
-
-        if not header:
+        serialIn = json.load(comm)
+        if not serialIn:
             return {}
-
-        data_time, data_type = struct.unpack('fc', bytes(header))
-
-        if data_type == 's':
-            state = struct.unpack('c', bytes(comm.read(1)))[0]
-            return {'time': data_time, 'type': 's', 'state': state}
-        elif data_type == 'u':
-            acc_x, acc_y, acc_z, bar_p, bar_alt, gps_lat, gps_lon = struct.unpack('fffffff', bytes(comm.read(28)))
-            return {'time': data_time, 'type': 'u', 'sensor': {'acc': {'x': acc_x, 'y': acc_y, 'z': acc_z}, 'bar': {'p': bar_p, 'alt': bar_alt}, 'gps': {'lat': gps_lat, 'lon': gps_lon}}}
-        else:
-            return {'time': data_time, 'type': data_type}
+        
+        return serialIn
+        
     except IOError:
         print('Flushing serial buffers...                                                                     ')
 
