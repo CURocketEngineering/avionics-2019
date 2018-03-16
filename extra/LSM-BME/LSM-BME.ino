fcc15cbad217ee46a4fd28efd1e12bc859490301
Foster McLane <fkmclane@gmail.com>
2018-03-16 09:03:13 -0400

    add LSM/BME test code

---
+++ extra/LSM-BME/LSM-BME.ino
@@ -0,0 +1,372 @@
+
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+#include <stdint.h>
+#include "SparkFunBME280.h"
+
+//Global sensor object
+BME280 mySensor;
+
+
+//////////////////////////
+// LSM9DS1 Library Init //
+//////////////////////////
+// Use the LSM9DS1 class to create an object. [imu] can be
+// named anything, we'll refer to that throught the sketch.
+LSM9DS1 imu;
+
+///////////////////////
+// Example I2C Setup //
+///////////////////////
+// SDO_XM and SDO_G are both pulled high, so our addresses are:
+#define LSM9DS1_M 0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+
+////////////////////////////
+// Sketch Output Settings //
+////////////////////////////
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+#define PRINT_SPEED 250 // 250 ms between prints
+
+// Earth's magnetic field varies by location. Add or subtract 
+// a declination to get a more accurate heading. Calculate 
+// your's here:
+// http://www.ngdc.noaa.gov/geomag-web/#declination
+#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+
+void setup() 
+{
+  
+  Serial.begin(115200);
+  
+  // Before initializing the IMU, there are a few settings
+  // we may need to adjust. Use the settings struct to set
+  // the device's communication mode and addresses:
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  // The above lines will only take effect AFTER calling
+  // imu.begin(), which verifies communication with the IMU
+  // and turns it on.
+  if (!imu.begin())
+  {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("Double-check wiring.");
+    Serial.println("Default settings in this sketch will " \
+                  "work for an out of the box LSM9DS1 " \
+                  "Breakout, but may need to be modified " \
+                  "if the board jumpers are.");
+  }
+
+    //***Driver settings********************************//
+  //commInterface can be I2C_MODE or SPI_MODE
+  //specify chipSelectPin using arduino pin names
+  //specify I2C address.  Can be 0x77(default) or 0x76
+  
+  //For I2C, enable the following and disable the SPI section
+  mySensor.settings.commInterface = I2C_MODE;
+  mySensor.settings.I2CAddress = 0x77;
+  
+  //For SPI enable the following and dissable the I2C section
+  //mySensor.settings.commInterface = SPI_MODE;
+  //mySensor.settings.chipSelectPin = 10;
+
+
+  //***Operation settings*****************************//
+  
+  //renMode can be:
+  //  0, Sleep mode
+  //  1 or 2, Forced mode
+  //  3, Normal mode
+  mySensor.settings.runMode = 3; //Normal mode
+  
+  //tStandby can be:
+  //  0, 0.5ms
+  //  1, 62.5ms
+  //  2, 125ms
+  //  3, 250ms
+  //  4, 500ms
+  //  5, 1000ms
+  //  6, 10ms
+  //  7, 20ms
+  mySensor.settings.tStandby = 0;
+  
+  //filter can be off or number of FIR coefficients to use:
+  //  0, filter off
+  //  1, coefficients = 2
+  //  2, coefficients = 4
+  //  3, coefficients = 8
+  //  4, coefficients = 16
+  mySensor.settings.filter = 0;
+  
+  //tempOverSample can be:
+  //  0, skipped
+  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+  mySensor.settings.tempOverSample = 1;
+
+  //pressOverSample can be:
+  //  0, skipped
+  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+    mySensor.settings.pressOverSample = 1;
+  
+  //humidOverSample can be:
+  //  0, skipped
+  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+  mySensor.settings.humidOverSample = 1;
+  
+  Serial.print("Program Started\n");
+  Serial.print("Starting BME280... result of .begin(): 0x");
+  
+  //Calling .begin() causes the settings to be loaded
+  delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
+  Serial.println(mySensor.begin(), HEX);
+
+  Serial.print("Displaying ID, reset and ctrl regs\n");
+  
+  Serial.print("ID(0xD0): 0x");
+  Serial.println(mySensor.readRegister(BME280_CHIP_ID_REG), HEX);
+  Serial.print("Reset register(0xE0): 0x");
+  Serial.println(mySensor.readRegister(BME280_RST_REG), HEX);
+  Serial.print("ctrl_meas(0xF4): 0x");
+  Serial.println(mySensor.readRegister(BME280_CTRL_MEAS_REG), HEX);
+  Serial.print("ctrl_hum(0xF2): 0x");
+  Serial.println(mySensor.readRegister(BME280_CTRL_HUMIDITY_REG), HEX);
+
+  Serial.print("\n\n");
+
+  Serial.print("Displaying all regs\n");
+  uint8_t memCounter = 0x80;
+  uint8_t tempReadData;
+  for(int rowi = 8; rowi < 16; rowi++ )
+  {
+    Serial.print("0x");
+    Serial.print(rowi, HEX);
+    Serial.print("0:");
+    for(int coli = 0; coli < 16; coli++ )
+    {
+      tempReadData = mySensor.readRegister(memCounter);
+      Serial.print((tempReadData >> 4) & 0x0F, HEX);//Print first hex nibble
+      Serial.print(tempReadData & 0x0F, HEX);//Print second hex nibble
+      Serial.print(" ");
+      memCounter++;
+    }
+    Serial.print("\n");
+  }
+  
+  
+  Serial.print("\n\n");
+  
+  Serial.print("Displaying concatenated calibration words\n");
+  Serial.print("dig_T1, uint16: ");
+  Serial.println(mySensor.calibration.dig_T1);
+  Serial.print("dig_T2, int16: ");
+  Serial.println(mySensor.calibration.dig_T2);
+  Serial.print("dig_T3, int16: ");
+  Serial.println(mySensor.calibration.dig_T3);
+  
+  Serial.print("dig_P1, uint16: ");
+  Serial.println(mySensor.calibration.dig_P1);
+  Serial.print("dig_P2, int16: ");
+  Serial.println(mySensor.calibration.dig_P2);
+  Serial.print("dig_P3, int16: ");
+  Serial.println(mySensor.calibration.dig_P3);
+  Serial.print("dig_P4, int16: ");
+  Serial.println(mySensor.calibration.dig_P4);
+  Serial.print("dig_P5, int16: ");
+  Serial.println(mySensor.calibration.dig_P5);
+  Serial.print("dig_P6, int16: ");
+  Serial.println(mySensor.calibration.dig_P6);
+  Serial.print("dig_P7, int16: ");
+  Serial.println(mySensor.calibration.dig_P7);
+  Serial.print("dig_P8, int16: ");
+  Serial.println(mySensor.calibration.dig_P8);
+  Serial.print("dig_P9, int16: ");
+  Serial.println(mySensor.calibration.dig_P9);
+  
+  Serial.print("dig_H1, uint8: ");
+  Serial.println(mySensor.calibration.dig_H1);
+  Serial.print("dig_H2, int16: ");
+  Serial.println(mySensor.calibration.dig_H2);
+  Serial.print("dig_H3, uint8: ");
+  Serial.println(mySensor.calibration.dig_H3);
+  Serial.print("dig_H4, int16: ");
+  Serial.println(mySensor.calibration.dig_H4);
+  Serial.print("dig_H5, int16: ");
+  Serial.println(mySensor.calibration.dig_H5);
+  Serial.print("dig_H6, int8: ");
+  Serial.println(mySensor.calibration.dig_H6);
+    
+  Serial.println();
+}
+
+void loop()
+{
+  printGyro();  // Print "G: gx, gy, gz"
+  printAccel(); // Print "A: ax, ay, az"
+  printMag();   // Print "M: mx, my, mz"
+  
+  // Print the heading and orientation for fun!
+  // Call print attitude. The LSM9DS1's magnetometer x and y
+  // axes are opposite to the accelerometer, so my and mx are
+  // substituted for each other.
+  printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+  Serial.println();
+
+    //Each loop, take a reading.
+  //Start with temperature, as that data is needed for accurate compensation.
+  //Reading the temperature updates the compensators of the other functions
+  //in the background.
+
+  Serial.print("Temperature: ");
+  Serial.print(mySensor.readTempC(), 2);
+  Serial.println(" degrees C");
+
+  Serial.print("Temperature: ");
+  Serial.print(mySensor.readTempF(), 2);
+  Serial.println(" degrees F");
+
+  Serial.print("Pressure: ");
+  Serial.print(mySensor.readFloatPressure(), 2);
+  Serial.println(" Pa");
+
+  Serial.print("Altitude: ");
+  Serial.print(mySensor.readFloatAltitudeMeters(), 2);
+  Serial.println("m");
+
+  Serial.print("Altitude: ");
+  Serial.print(mySensor.readFloatAltitudeFeet(), 2);
+  Serial.println("ft"); 
+
+  Serial.print("%RH: ");
+  Serial.print(mySensor.readFloatHumidity(), 2);
+  Serial.println(" %");
+  
+  Serial.println();
+  
+  delay(PRINT_SPEED);
+}
+
+void printGyro()
+{
+  // To read from the gyroscope, you must first call the
+  // readGyro() function. When this exits, it'll update the
+  // gx, gy, and gz variables with the most current data.
+  imu.readGyro();
+  
+  // Now we can use the gx, gy, and gz variables as we please.
+  // Either print them as raw ADC values, or calculated in DPS.
+  Serial.print("G: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcGyro helper function to convert a raw ADC value to
+  // DPS. Give the function the value that you want to convert.
+  Serial.print(imu.calcGyro(imu.gx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gy), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gz), 2);
+  Serial.println(" deg/s");
+#elif defined PRINT_RAW
+  Serial.print(imu.gx);
+  Serial.print(", ");
+  Serial.print(imu.gy);
+  Serial.print(", ");
+  Serial.println(imu.gz);
+#endif
+}
+
+void printAccel()
+{
+  // To read from the accelerometer, you must first call the
+  // readAccel() function. When this exits, it'll update the
+  // ax, ay, and az variables with the most current data.
+  imu.readAccel();
+  
+  // Now we can use the ax, ay, and az variables as we please.
+  // Either print them as raw ADC values, or calculated in g's.
+  Serial.print("A: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcAccel helper function to convert a raw ADC value to
+  // g's. Give the function the value that you want to convert.
+  Serial.print(imu.calcAccel(imu.ax), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ay), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.az), 2);
+  Serial.println(" g");
+#elif defined PRINT_RAW 
+  Serial.print(imu.ax);
+  Serial.print(", ");
+  Serial.print(imu.ay);
+  Serial.print(", ");
+  Serial.println(imu.az);
+#endif
+
+}
+
+void printMag()
+{
+  // To read from the magnetometer, you must first call the
+  // readMag() function. When this exits, it'll update the
+  // mx, my, and mz variables with the most current data.
+  imu.readMag();
+  
+  // Now we can use the mx, my, and mz variables as we please.
+  // Either print them as raw ADC values, or calculated in Gauss.
+  Serial.print("M: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcMag helper function to convert a raw ADC value to
+  // Gauss. Give the function the value that you want to convert.
+  Serial.print(imu.calcMag(imu.mx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.my), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.mz), 2);
+  Serial.println(" gauss");
+#elif defined PRINT_RAW
+  Serial.print(imu.mx);
+  Serial.print(", ");
+  Serial.print(imu.my);
+  Serial.print(", ");
+  Serial.println(imu.mz);
+#endif
+}
+
+// Calculate pitch, roll, and heading.
+// Pitch/roll calculations take from this app note:
+// http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
+// Heading calculations taken from this app note:
+// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
+void printAttitude(
+float ax, float ay, float az, float mx, float my, float mz)
+{
+  float roll = atan2(ay, az);
+  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+  
+  float heading;
+  if (my == 0)
+    heading = (mx < 0) ? 180.0 : 0;
+  else
+    heading = atan2(mx, my);
+    
+  heading -= DECLINATION * PI / 180;
+  
+  if (heading > PI) heading -= (2 * PI);
+  else if (heading < -PI) heading += (2 * PI);
+  else if (heading < 0) heading += 2 * PI;
+  
+  // Convert everything from radians to degrees:
+  heading *= 180.0 / PI;
+  pitch *= 180.0 / PI;
+  roll  *= 180.0 / PI;
+  
+  Serial.print("Pitch, Roll: ");
+  Serial.print(pitch, 2);
+  Serial.print(", ");
+  Serial.println(roll, 2);
+  Serial.print("Heading: "); Serial.println(heading, 2);
+}
