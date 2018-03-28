ea69bc9b8ea5b36a2e163b029a9ae5584f5d433e
HarrisonHall <harrisonchristianhall@gmail.com>
2018-03-28 16:01:54 -0400

    urwid base

---
+++ base/urw.py
@@ -0,0 +1,24 @@
+
+import urwid
+import term
+
+##
+def init():
+    #
+    pass
+
+##
+def update():
+    #
+    pass
+
+##
+def graph():
+    #
+    pass
+
+
+init()
+while(True):
+    update()
+    graph()
