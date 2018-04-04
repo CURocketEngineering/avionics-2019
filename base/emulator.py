cd7844c0c1d77c7b011e43044e56a9973f548511
JackyFWong <jwong2398@gmail.com>
2018-04-04 19:11:06 -0400

    add emulator for csv data

--- base/emulator.py
+++ base/emulator.py
@@ -1,14 +1,15 @@
 # Feeds the urw.py fake data from a csv
 
 import csv
+import json
 
 data = {'time': -1, 'state': 'no_comm', 'sensors': None}
 
-def giveData():
-    with open('flight.py', 'rb') as csvfile:
+def giveData(int i):
+    with open('flight.csv', 'rb') as csvfile:
         dataReader = csv.reader(csvfile)
         for row in dataReader:
-            if (isinstance(row, str) == False):
+            if (row = i):
                 data['time'] = row[0]
                 data['state'] = row[1]
                 data['sensors']['acc']['z'] = row[17]
