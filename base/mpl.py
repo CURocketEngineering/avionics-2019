8d421a778bb65a8f34fa8bcea6cbbbb10c62074c
HarrisonHall <harrisonchristianhall@gmail.com>
2018-04-06 18:45:51 -0400

    plotting code

--- base/mpl.py
+++ base/mpl.py
@@ -1,24 +1,41 @@
+##Use python3, requires matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
+import math
 
+##Used for Testing
+z = 0
+x = [0]
+y = [0]
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
-z = 0
-global z
-
-def animate(i):
+def fakeAnimate(var):
     #pullData = open("sampleText.txt","r").read()
     #dataArray = pullData.split('\n')
-    x = []
-    y = []
-    '''for eachLine in dataArray:
-        if len(eachLine)>1:
-            x,y = eachLine.split(',')
-            xar.append(int(x))
-            yar.append(int(y))'''
+    global z
+    x.append(z)
+    y.append(math.pow(z,2))
     ax1.clear()
-    ax1.plot(xar,yar)
+    ax1.plot(x,y)
     z += 1
-ani = animation.FuncAnimation(fig, animate, interval=1000)
-plt.show()
+    time.sleep(1)
+
+##Real functions
+#Functions for animating altitude and acceleration
+graphs = plt.figure()
+plot_acc = graphs.add_subplot(1,2,1)
+plot_acc.xlabel("time, s")
+plot_acc.ylabel("g's")
+plot_acc.title("Acceleration")
+
+plot_alt = graphs.add_subplot(1,2,2)
+plot_acc.xlabel("time, s")
+plot_acc.ylabel("ft")
+plot_acc.title("Altitude")
+
+#update function
+
+
+update = animation.FuncAnimation(graphs, updateFunction, interval=1000)
+graphs.show()
