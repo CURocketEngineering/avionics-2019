3badc222825aa49a2715b93da275ba05265c58e4
HarrisonHall <harrisonchristianhall@gmail.com>
2018-04-08 21:23:11 -0400

    Some experimentation with mpl

--- base/mpl.py
+++ base/mpl.py
@@ -3,21 +3,24 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 import math
+import sys
+import comm as comm
+from serial import Serial 
 
 ##Used for Testing
-z = 0
+z = 1
+'''
 x = [0]
 y = [0]
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
+'''
 def fakeAnimate(var):
-    #pullData = open("sampleText.txt","r").read()
-    #dataArray = pullData.split('\n')
     global z
-    x.append(z)
-    y.append(math.pow(z,2))
-    ax1.clear()
-    ax1.plot(x,y)
+    arr_sec.append(z)
+    arr_alt.append(math.pow(z,2))
+    plot_acc.clear()
+    plot_acc.plot(arr_sec,arr_acc)
     z += 1
     time.sleep(1)
 
@@ -31,7 +34,6 @@ def animate(i):
     global arr_acc
     global arr_alt
     global arr_sec
-    
     message = comm.read()
     if message:
         count = 0
@@ -55,7 +57,9 @@ def animate(i):
     plot_alt.clear()
     plot_alt.plot(arr_sec,arr_alt)
     
+    
 graphs = plt.figure()
+graphs.canvas.set_window_title("THE AVIONICS GUYS ARE AWESOME")
 plot_acc = graphs.add_subplot(1,2,1)
 plot_acc.set_xlabel("time, s")
 plot_acc.set_ylabel("g's")
@@ -66,9 +70,15 @@ plot_acc.set_xlabel("time, s")
 plot_acc.set_ylabel("ft")
 plot_acc.set_title("Altitude")
 
+
 #update function
 
-
-update = animation.FuncAnimation(graphs, animate, interval=200)
-graphs.show()
-
+try:
+    comm.init()
+    update = animation.FuncAnimation(graphs, animate, interval=200)
+    plt.show()
+except:
+    print("There was an error with comm, graphs will not operate.")
+    print("I recommend restarting, unless this is a test")
+    update = animation.FuncAnimation(graphs, fakeAnimate,interval=200)
+    plt.show()
